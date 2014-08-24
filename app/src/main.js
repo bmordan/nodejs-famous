0df7b647b193a497025d52609f31d028c0311bc7
Bernard Mordan <bernardmordan@gmail.com>
2014-08-24 15:37:53 +0100

    created simple brick slide interface, first attempt at wiring in dgram network socket

--- app/src/main.js
+++ app/src/main.js
@@ -5,25 +5,45 @@ define(function(require, exports, module) {
     var Engine = require('famous/core/Engine')
     var View = require('famous/core/View')
     var Modifier = require('famous/core/Modifier')
+    var StateModifier = require('famous/modifiers/StateModifier')
     var Transform = require('famous/core/Transform')
+    var Surface = require('famous/core/Surface')
     var ImageSurface = require('famous/surfaces/ImageSurface')
+    var TouchSync = require('famous/inputs/TouchSync')
+    // create socket
     
     var mainContext = Engine.createContext()
+    var sync = new TouchSync()
+    var position = [0,0]
     
     function _createBrick(){
       var brick = new View()
       var brickMod = new Modifier({
         origin: [0.5,0.5],
-        align: [0.5,0.5]
+        align: [0.5,0.5],
+        transform: function(){
+          return Transform.translate(position[0], position[1], 0)
+        }
       })
       var brickSurface = new ImageSurface({
         size: [200,200],
         content: 'content/images/2.png'
       })
+      //events
+      brickSurface.pipe(sync)
+      sync.on('update', function(data){
+        position[0] += data.delta[0]
+      })
+      sync.on('end', function(data){ 
+        var min = 0 - mainContext.getSize()[0]/2
+        var max = mainContext.getSize()[0]/2
+        if(position[0] < min) position[0] = min+75
+        if(position[0] > max) position[0] = max-75
+      })
+      //render
       brick.add(brickMod).add(brickSurface)
-      return mainContext.add(brick)
+      mainContext.add(brick)
     }
     
     _createBrick()
-    
 });
